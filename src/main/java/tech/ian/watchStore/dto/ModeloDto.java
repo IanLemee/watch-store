fb9d52ef5c55555ef2235db6427567262da2eded
Ian Leme <ianlemee@hotmail.com>
2024-06-23 19:23:29 -0300

    feat: adicionando marcaId ao dto

--- src/main/java/tech/ian/watchStore/dto/ModeloDto.java
+++ src/main/java/tech/ian/watchStore/dto/ModeloDto.java
@@ -3,9 +3,12 @@ package tech.ian.watchStore.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+import java.util.UUID;
+
 public record ModeloDto(@NotBlank(message = "O Campo deve ser preenchido") String name,
                         @NotBlank(message = "O Campo deve ser preenchido") String description,
                         @NotBlank(message = "O Campo deve ser preenchido") String releaseDate,
                         @NotNull(message = "O Campo deve ser preenchido") Double price,
-                        @NotBlank(message = "O Campo deve ser preenchido") String image){
+                        @NotBlank(message = "O Campo deve ser preenchido") String image,
+                        UUID marcaId){
 }
