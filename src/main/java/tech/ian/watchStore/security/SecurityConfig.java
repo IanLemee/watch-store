0621194170b07b627fe45d36a8037edbad9d5028
Ian Leme <ianlemee@hotmail.com>
2024-07-15 21:07:27 -0300

    feat: insercao de novas camadas de configuracao

--- src/main/java/tech/ian/watchStore/security/SecurityConfig.java
+++ src/main/java/tech/ian/watchStore/security/SecurityConfig.java
@@ -1,22 +1,55 @@
 package tech.ian.watchStore.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private SecurityFilter securityFilter;
+
+    private static final String[] SWAGGER_LIST = {
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/swagger-resources/**"
+    };
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/marca/").authenticated();
-                    auth.requestMatchers("/modelo/").authenticated();
+                    auth.requestMatchers(HttpMethod.POST, "/user/register").permitAll()
+                            .requestMatchers(HttpMethod.GET, "/user/verify").permitAll()
+                            .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/marca/").hasRole("ADMIN")
+                    .requestMatchers(HttpMethod.DELETE, "/modelo/").hasRole("ADMIN")
+                            .requestMatchers(SWAGGER_LIST).permitAll();
                     auth.anyRequest().permitAll();
-                });
+                }).addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
